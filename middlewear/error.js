a71eebe4bbfb9eec0b55b05d018d31da725869d5
Lars Lien <lars@familienlien.com>
2019-06-25 09:04:49 +0200

    addded error and admin middlewear

---
+++ middlewear/error.js
@@ -0,0 +1,21 @@
+//const winston = require("winston"); //TODO implemet a logger
+
+//Middlewear that catches all asyncronus errors in the requst processing pipline
+module.exports = function(err, req, res, next) {
+  //winston.error("Something failed in express: ", err); //TODO implemet a logger
+  console.log("something failed in express", err);
+  res.status(500).send("Something failed");
+};
+
+//Logging levels:
+
+/*  
+
+error - 1
+warn - 2
+info - 3
+verbose - 4
+debug - 5
+silly - 6
+
+*/
--- startup/routes.js
+++ startup/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const activities = require("../routes/activities");
 const auth = require("../routes/authentication/auth");
 const users = require("../routes/authentication/users");
+const error = require("../middlewear/error");
 
 module.exports = function(app) {
   app.use(express.json());
@@ -9,5 +10,5 @@ module.exports = function(app) {
   app.use("/api/auth", auth);
   app.use("/api/users", users);
 
-  //app.use(error); // Add error middlewear ??
+  app.use(error);
 };
