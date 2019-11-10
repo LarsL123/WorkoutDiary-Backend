ce6414645658868ae10f2f0ca9c70d56d7f47ee9
Lars Lien <Lars@familienlien.com>
2019-11-10 21:27:51 +0100

    Updation database to cloud hosted. Now Checking if circleCI build works

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports.startDB = function() {
-  const database = config.get("db");
+  const database = config.get("db");  //TODO IMPORTANT -- Don't include the username/password in the deafult string.
   mongoose
     .connect(database, {
       useCreateIndex: true,
@@ -13,6 +13,7 @@ module.exports.startDB = function() {
     .then(() => console.log(`Connected to ${database}..`)) //TODO Change to default logger
     .catch(err => {
       console.error(`Was not able to connect to ${database}, shutting down:`); //TODO Change to default logger
+      console.error(err);
       process.exit(1);
     });
 };
