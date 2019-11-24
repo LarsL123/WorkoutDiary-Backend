fafa6c8be8f450d2169bf84b88b3d66e18d81813
Lars Lien <Lars@familienlien.com>
2019-11-24 22:23:31 +0100

    Revert "Added cors libary to enable connection'"
    This reverts commit 679f29140f7c8be6395cacd29216e41dc657c723.

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const app = express();
-const cors = require("cors"); // --> https://medium.com/@alexishevia/using-cors-in-express-cac7e29b005b
 
 //TODO implememt winston logger:
 // const logger = require("./services/logger"); */
 // logger.init();
-app.use(cors());
+
 require("./startup/config")();
 require("./startup/db").startDB();
 require("./startup/routes")(app);
 require("./startup/validation")();
 require("./startup/production")(app); //TODO: Need to read more about helmet...
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 const server = app.listen(
   port,
   () => console.log(`Listening on port ${port}...`) // Change to winston logger
